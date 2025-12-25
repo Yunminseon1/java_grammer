2c9d32ebf115ca52b987124953eb6fd38dea4757
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-25 17:40:40 +0900

    Update C01innerClass.java

--- src/C05AnonymousLamda/C01innerClass.java
+++ src/C05AnonymousLamda/C01innerClass.java
@@ -1,20 +1,21 @@
 package C05AnonymousLamda;
 
 
-// 내부 클래스 : 클래스안에 클래스의 존재
-//1) static 내부클래스 2) 익명내부클래스
 
+//  innerClass(내부 클래스) : 클래스안에 클래스의 존재
 public class C01innerClass {
     static void main(String[] args) {
+
+// 1) static 내부클래스 2) 익명내부클래스
 //    AbstractAnimal를 상속한 클래스가 별도로 존재하지않고,
 //    내부에 익명클래스가 만들어짐과 동시에 익명객체가 생성
-//     AbstractAnimal aa = new AbstractAnimal(){
-//        @Override
-//            void makeSound() {
-//                System.out.println("멍멍");
-//            }
-//        };
-//
+    AbstractAnimal aa = new AbstractAnimal(){
+       @Override
+           void makeSound() {
+               System.out.println("멍멍");
+           }
+       };
+
         Member.MemberInner mi = new Member.MemberInner(20);
         System.out.println(mi.getB());
     }
@@ -31,8 +32,7 @@ class Member{
         this.a = a;
     }
 
-//    static 내부클래스 : Member의 static 변수처럼 활용
-
+   // static 내부클래스 : Member의 static 변수처럼 활용
     static class MemberInner{
         private int b;
 
@@ -44,6 +44,4 @@ class Member{
         }
 
     }
-
-
 }
