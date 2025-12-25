2e0c719803589d481b7076abdd0477732fc9c988
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-25 17:18:47 +0900

    Update MyThread.java

--- src/C08Thread/MyThread.java
+++ src/C08Thread/MyThread.java
@@ -3,11 +3,12 @@ package C08Thread;
 public class MyThread extends Thread{
 
     public void run(){
-//        스레드를 실행(.start) 하면 Thread 클래스 내에 run메서드 호출
-//        그런데, Thread 클래스의 run메서드는 비어있으므로, 상속받아 오버라이딩한다.
-//        @Override
-//        public void run(){
-//            System.out.println("스레드 실행");
-//        }
+       // 스레드를 실행(.start) 하면 Thread 클래스 내에 run메서드 호출
+       // 그런데, Thread 클래스의 run메서드는 비어있으므로, 상속받아 오버라이딩한다.
+       @Override
+       public void run(){
+           System.out.println("스레드 실행");
+       }
     }
 }
+
