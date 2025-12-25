afbbef61372c6ea0fdc93d14649ffa7bb05329a8
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-26 00:46:18 +0900

    Update C02Calender.java

--- src/C06EtcClass/C02Calender.java
+++ src/C06EtcClass/C02Calender.java
@@ -34,25 +34,25 @@ public class C02Calender {
        System.out.println(myLocalTime.getMinute());
        System.out.println(myLocalTime.getSecond());
 
-        LocalDateTime myLocalDateTime = LocalDateTime.now();
-        System.out.println(myLocalDateTime.getYear());
-        System.out.println(myLocalDateTime.getHour());
+       LocalDateTime myLocalDateTime = LocalDateTime.now();
+       System.out.println(myLocalDateTime.getYear());
+       System.out.println(myLocalDateTime.getHour());
 
-        // ChronoField 매개변수로 다양한 형식의 날짜/시간정보 조회
-        System.out.println(myLocalDateTime.get(ChronoField.YEAR));
-        System.out.println(myLocalDateTime.get(ChronoField.MONTH_OF_YEAR));
-        System.out.println(myLocalDateTime.get(ChronoField.AMPM_OF_DAY)); // 0: 오전 1: 오후
-        System.out.println(myLocalDateTime.get(ChronoField.CLOCK_HOUR_OF_DAY)); // 24시 체계
+       // ChronoField 매개변수로 다양한 형식의 날짜/시간정보 조회
+       System.out.println(myLocalDateTime.get(ChronoField.YEAR));
+       System.out.println(myLocalDateTime.get(ChronoField.MONTH_OF_YEAR));
+       System.out.println(myLocalDateTime.get(ChronoField.AMPM_OF_DAY)); // 0: 오전 1: 오후
+       System.out.println(myLocalDateTime.get(ChronoField.CLOCK_HOUR_OF_DAY)); // 24시 체계
 
-        // .of : 임의로 특정시간정보객체를 만들어내고싶을때 사용
-        LocalDate birthDay = LocalDate.of(2000, 11,12);
-        System.out.println(birthDay);
+       // .of : 임의로 특정시간정보객체를 만들어내고싶을때 사용
+       LocalDate birthDay = LocalDate.of(2000, 11,12);
+       System.out.println(birthDay);
 
-        LocalDateTime birthDayTime = LocalDateTime.of(2000, 11,12,15,14,10);
-        System.out.println(birthDayTime);
-
+       LocalDateTime birthDayTime = LocalDateTime.of(2000, 11,12,15,14,10);
+       System.out.println(birthDayTime);
     }
 }
+
 
 
 
