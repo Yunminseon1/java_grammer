5c474816f01fcd1350e27dc779d3f0b7e010a672
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-25 17:52:22 +0900

    Update C01Inheritance.java

--- src/C03Inheritance/C01Inheritance.java
+++ src/C03Inheritance/C01Inheritance.java
@@ -1,25 +1,25 @@
 package C03Inheritance;
 
 import com.sun.java.accessibility.util.AccessibilityListenerList;
-//상속 : 부모의 변수와 메서드를 자식클래스에서 물려받는것.(extends 키워드사용)
+// 상속 : 부모의 변수와 메서드를 자식클래스에서 물려받는것.(extends 키워드사용)
 public class C01Inheritance extends Parents {
     int b = 20;
     public static void main(String[] args) {
         C01Inheritance c1 = new C01Inheritance();
 
-//        부모클래스의 a변수 상속
+       // 부모클래스의 a변수 상속
         System.out.println(c1.a);
         System.out.println(c1.b);
 
-//        private 접근제어자는 클래스 내부에서만 접근가능하므로, 자식클래스에서도 접근불가하다.
-//        System.out.println(c1.c);
-//        부모클래스의 m1메서드 상속
+       // private 접근제어자는 클래스 내부에서만 접근가능하므로, 자식클래스에서도 접근불가
+       // System.out.println(c1.c);
+       // 부모클래스의 m1메서드 상속
         c1.m1();
         c1.m2();
     }
 
-//   오버라이딩(부모 메서드의 재정의) : 부모클래스의 메서드명과 자식클래스의 메서드명이 동일한 경우
-//   오버라이드된 메서드임을 명시적으로 표현하는 키워드(어노테이션) 성능최적화를 위해서 해당 키워드 붙여주는것이 더좋다.
+// 오버라이딩(부모 메서드의 재정의) : 부모클래스의 메서드명과 자식클래스의 메서드명이 동일한 경우
+// 오버라이드된 메서드임을 명시적으로 표현하는 키워드(어노테이션) 성능최적화를 위해서 해당 키워드 붙여주는것이 더좋다.
 
 @Override
     public void m1(){
@@ -38,3 +38,4 @@ class Parents{
         System.out.println("부모 클래스 입니다.");
     }
 }
+
