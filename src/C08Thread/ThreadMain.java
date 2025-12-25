70809918513de697b6f6d8f3196dfa7e9e676f1c
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-26 00:42:59 +0900

    Update ThreadMain.java

--- src/C08Thread/ThreadMain.java
+++ src/C08Thread/ThreadMain.java
@@ -6,21 +6,21 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadMain {
     static void main(String[] args) throws InterruptedException {
-//        단일 스레드 실행
+       // 단일 스레드 실행
         for(int i = 0; i<1000; i++){
             Library.borrow();
         }
         System.out.println(Library.getBookCount());
 
-//        스레드 생성
-//        방법1. 스레드 클래스 상속 방식
+       // 스레드 생성
+       // 방법1. 스레드 클래스 상속 방식
         Thread t1 = new MyThread();
         Thread t2 = new MyThread();
         Thread t3 = new MyThread();
         Thread t4 = new MyThread();
         Thread t5 = new MyThread();
 
-//      각 스레드는 코드의 실행순서가 보장되지 않는 비동기적 실행(main에 할당된 thread 포함)
+     // 각 스레드는 코드의 실행순서가 보장되지 않는 비동기적 실행(main에 할당된 thread 포함)
         t1.start();
         t2.start();
         t3.start();
@@ -29,26 +29,26 @@ public class ThreadMain {
         System.out.println("hello world");
 
 
-//        방법2. Runnable을 구현한 객체를 Thread 클래스에 주입하는 방식
-//        구현 객체의 run메서드를 Thread객체가 가져다 사용
+       // 방법2. Runnable을 구현한 객체를 Thread 클래스에 주입하는 방식
+       // 구현 객체의 run메서드를 Thread객체가 가져다 사용
         new Thread(() -> System.out.println("스레드 실행1")).start();
         new Thread(() -> System.out.println("스레드 실행2")).start();
         new Thread(() -> System.out.println("스레드 실행3")).start();
         new Thread(() -> System.out.println("스레드 실행4")).start();
         System.out.println("hello world");
 
-//        멀티스레드 동시성 이슈 테스트
-//        스레드 1000개 library.borrrow를 넣기
+       // 멀티스레드 동시성 이슈 테스트
+       // 스레드 1000개 library.borrrow를 넣기
         for (int i = 0; i<1000; i++){
             Thread t1 = new Thread(()->Library.borrow());
             t1.start();
-//            join : 한 스레드의 작업이 모두 완료될때까지 다른 스레드를 실행하지않는것 -> 사실상 단일 스레드처럼 동작하므로 성능저하가 발생한다.
+           // join : 한 스레드의 작업이 모두 완료될때까지 다른 스레드를 실행하지않는것 -> 사실상 단일 스레드처럼 동작하므로 성능저하가 발생한다.
             t1.join();
         }
         Thread.sleep(30000);
         System.out.println(Library.getBookCount());
 
-//        synchronized를 통한 동시성제어예시
+       // synchronized를 통한 동시성제어예시
         StringBuffer sb1 = new StringBuffer();
         sb1.append("hello"); // synchronized 메서드
         StringBuilder sb2 = new StringBuilder();
@@ -61,3 +61,4 @@ public class ThreadMain {
 
     }
 }
+
