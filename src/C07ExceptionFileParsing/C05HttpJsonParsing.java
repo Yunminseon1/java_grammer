2d3f461a6794c2fcd1786e98a0b5797ae9ef95ae
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-25 17:28:39 +0900

    Update C05HttpJsonParsing.java

--- src/C07ExceptionFileParsing/C05HttpJsonParsing.java
+++ src/C07ExceptionFileParsing/C05HttpJsonParsing.java
@@ -13,22 +13,23 @@ import java.util.List;
 
 public class C05HttpJsonParsing {
     public static void main(String[] args) throws IOException, InterruptedException {
-////        외부에서 데이터를 받아 java의 객체로 변환
-////        외부api요청을 하기 위한 라이브러리 : HttpClient, RestTemplate, RestClient
-////        HttpClient는 java내장클래스
-////        RestTemplate, RestClient는 스프링에서 가장 많이 사용하는 클래스.
-//        HttpClient client = HttpClient.newHttpClient();
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1"))
-//                .GET().build();
-//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        String data = response.body();
-//        ObjectMapper o1 = new ObjectMapper();
-////        객체로 파싱
-//        Post p1 = o1.readValue(data, Post.class);
-//        System.out.println(p1);
+       // 외부에서 데이터를 받아 java의 객체로 변환
+       // 외부api요청을 하기 위한 라이브러리 : HttpClient, RestTemplate, RestClient
+       // HttpClient는 java내장클래스
+       // RestTemplate, RestClient는 스프링에서 가장 많이 사용하는 클래스
+       HttpClient client = HttpClient.newHttpClient();
+       HttpRequest request = HttpRequest.newBuilder()
+               .uri(URI.create("https://jsonplaceholder.typicode.com/posts/1"))
+               .GET().build();
+       HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+       String data = response.body();
+       ObjectMapper o1 = new ObjectMapper();
+        
+       // 객체로 파싱
+       Post p1 = o1.readValue(data, Post.class);
+       System.out.println(p1);
 
-////        리스트형식의 객체로 파싱
+       // 리스트형식의 객체로 파싱
         ObjectMapper o1 = new ObjectMapper();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request2 = HttpRequest.newBuilder()
@@ -45,6 +46,7 @@ public class C05HttpJsonParsing {
         System.out.println(postList);
     }
 }
+
 class Post{
     private long id;
     private long userId;
@@ -76,4 +78,5 @@ class Post{
                 ", body='" + body + '\'' +
                 '}';
     }
+
 }
