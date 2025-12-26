322f22070d0666b4ff25e610fbf20c70d03c9b72
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-26 18:36:18 +0900

    Refactor C01Reflection.java for clarity and structure

--- src/C10Annotation/C01Reflection.java
+++ src/C10Annotation/C01Reflection.java
@@ -4,9 +4,9 @@ import java.lang.reflect.Field;
 
 public class C01Reflection {
     static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-//        리플렉션 : 자바에서 런타임시점에 클래스, 변수, 메서드에 접근할수있는 기술
+       // 리플렉션 : 자바에서 런타임시점에 클래스, 변수, 메서드에 접근할수있는 기술
         Person p1 = new Person();
-//        아래와 같은 방식을 통해 private변수인 name변수에 직접 접근
+       // 아래와 같은 방식을 통해 private변수인 name변수에 직접 접근
         Field nameField = Person.class.getDeclaredField("name");
         nameField.trySetAccessible(); // private 이라 할지라도 접근가능하도록 세팅 가능.
         nameField.set(p1,"hongildong");
@@ -25,5 +25,6 @@ class Person{
                 ", age=" + age +
                 '}';
     }
+
 
 }
