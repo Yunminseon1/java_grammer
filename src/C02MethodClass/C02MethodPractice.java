31795dfede28c31def8a4d529b20a00da06e4185
Yunminseon1 <dbsalstjssla@naver.com>
2025-12-26 17:10:57 +0900

    Update C02MethodPractice.java

--- src/C02MethodClass/C02MethodPractice.java
+++ src/C02MethodClass/C02MethodPractice.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class C02MethodPractice {
     static void main(String[] args) {
 
-//        소수판별기 메서드(isprime) 생성
-//        접근제어자(public), 클래스메서드, 반환타입(boolean), 입력값은 int
+       // 소수판별기 메서드(isprime) 생성
+       // 접근제어자(public), 클래스메서드, 반환타입(boolean), 입력값은 int
         Scanner sc = new Scanner(System.in);
         int input = Integer.parseInt(sc.nextLine());
         if(isPrime(input)){
@@ -20,7 +20,7 @@ public class C02MethodPractice {
     public static  boolean  isPrime(int num) {
         boolean isPrime = true;
         if (num<=1){
-//            메서드에서 return을 만나면 메서드가 강제종료
+           // 메서드에서 return을 만나면 메서드가 강제종료
             return false;
         }
         for (int i =2; i*i <=num; i++){
@@ -32,10 +32,11 @@ public class C02MethodPractice {
     }
     public static void voidMethod(){
         if(true){
-//            void 메서드에서 return은 메서드 강제종료 목적으로 사용
+           // void 메서드에서 return은 메서드 강제종료 목적으로 사용
             return;
         }
         System.out.println("hello world");
     }
 }
+
 
